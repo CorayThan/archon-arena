ecfd762195059e8bf08d2eebe214873439b932d1
Grant Titus <grant@granttitus.com>
2019-07-08 20:06:26 -0400

    Add missing render call

--- src/game/GameScene.tsx
+++ src/game/GameScene.tsx
@@ -614,6 +614,7 @@ class GameScene extends Phaser.Scene {
             card.destroy()
             this.render()
         }
+        this.render()
     }
 
     onMouseOverUpgrade(e: MouseEvent, target: any) {
