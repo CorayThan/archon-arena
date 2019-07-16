4dcd7b261dbff8c7558a5948066ac1eed451b50a
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2019-07-16 12:02:27 -0500

    added all the card-script files

---
+++ src/card-scripts/cota/dis/GatewayToDis.ts
@@ -0,0 +1,11 @@
+import { CardScript } from "../../types/CardScript"
+import { cardScripts } from "../../types/CardScripts"
+
+const cardScript: CardScript = {
+    amber: () =>  0,
+    onPlay: {
+
+    }
+}
+
+cardScripts.scripts.set("gateway-to-dis", cardScript)
