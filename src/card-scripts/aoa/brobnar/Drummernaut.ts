ad0553c7982f2669f29de9ecbe9f0b4bf94e5581
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2019-07-17 11:32:57 -0500

    untamed Stuff

---
+++ src/card-scripts/aoa/brobnar/Drummernaut.ts
@@ -0,0 +1,49 @@
+import {cardScripts} from "../../types/CardScripts"
+import {CardScript} from "../../types/CardScript"
+import {Creature} from "../../../shared/gamestate/Creature"
+import {friendlyCreatures, putInHand} from "../../types/ScriptUtils"
+
+const cardScript: CardScript = {
+    //TODO would be nice to not have to duplicate this three times over, without making a dedicated function in ScriptUtils for this...
+    onPlay: {
+        validTargets: (state, config) => {
+            const friendlies = friendlyCreatures(state)
+            return friendlies
+                .filter(creature => creature.traits.includes("Giant") && creature.id !== config.thisCard.id)
+
+        },
+        numberOfTargets: () => 1,
+        perform: (state, config) => {
+            const targetedGiant = config.targets[0] as Creature
+            putInHand(targetedGiant)
+        }
+    },
+    fight: {
+        validTargets: (state, config) => {
+            const friendlies = friendlyCreatures(state)
+            return friendlies
+                .filter(creature => creature.traits.includes("Giant") && creature.id !== config.thisCard.id)
+
+        },
+        numberOfTargets: () => 1,
+        perform: (state, config) => {
+            const targetedGiant = config.targets[0] as Creature
+            putInHand(targetedGiant)
+        }
+    },
+    reap: {
+        validTargets: (state, config) => {
+            const friendlies = friendlyCreatures(state)
+            return friendlies
+                .filter(creature => creature.traits.includes("Giant") && creature.id !== config.thisCard.id)
+
+        },
+        numberOfTargets: () => 1,
+        perform: (state, config) => {
+            const targetedGiant = config.targets[0] as Creature
+            putInHand(targetedGiant)
+        }
+    }
+}
+
+cardScripts.scripts.set("drummernaut", cardScript)
---
+++ src/card-scripts/aoa/brobnar/Ogopogo.ts
@@ -0,0 +1,17 @@
+import {CardScript} from "../../types/CardScript"
+import {Creature} from "../../../shared/gamestate/Creature"
+import {allCreatures, dealDamage} from "../../types/ScriptUtils"
+
+const cardScript: CardScript = {
+    power: () => 6,
+    fight: {
+        //TODO if destroyed the creature it fought... choose a target
+        validTargets: allCreatures,
+        numberOfTargets: () => 1,
+        perform: (state, config) => {
+            dealDamage(config.targets[0] as Creature, 2)
+        }
+    }
+}
+
+//cardScripts.scripts.set("ogopogo", cardScript)
