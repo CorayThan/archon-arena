9b6c4cf403017d5d2ba904189eb80c64874d08e0
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2019-07-22 11:21:23 -0500

    rest

---
+++ src/card-scripts/cota/dis/MindBarb.ts
@@ -0,0 +1,25 @@
+import {CardScript} from "../../types/CardScript"
+import {cardScripts} from "../../CardScripts"
+import {GameState, PlayerState} from "../../../shared/gamestate/GameState"
+import {inactivePlayerState} from "../../ScriptUtils"
+import {removeCardFromHand} from "../../../game/StateUtils"
+
+const cardScript: CardScript = {
+    amber: () => 1,
+    onPlay: {
+        perform: (state) => {
+            const opponentState = inactivePlayerState(state) as PlayerState
+
+            if (!opponentState.hand.length) {
+                return
+            }
+
+            const randomCardNum = Math.floor(Math.random() * opponentState.hand.length)
+            const randomCard = opponentState.hand[randomCardNum]
+            removeCardFromHand(opponentState, opponentState.hand[randomCardNum].id)
+            opponentState.discard.push(randomCard)
+        }
+    }
+}
+
+cardScripts.scripts.set("mind-barb", cardScript)
