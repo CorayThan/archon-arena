5707fe18152cb49b4bd7ce5eb25c4c15c5419922
skyjedi <50d2037077b7240576851289a37ce14e5c911642>
2019-07-24 21:21:31 -0500

    the scrum

--- src/card-scripts/cota/dis/MindBarb.ts
+++ src/card-scripts/cota/dis/MindBarb.ts
@@ -1,25 +1,14 @@
-import { CardScript } from "../../types/CardScript"
+import { CardScript, CardActionConfig } from "../../types/CardScript"
 import { cardScripts } from "../../CardScripts"
-import { GameState, PlayerState } from "../../../shared/gamestate/GameState"
-import { inactivePlayerState } from "../../ScriptUtils"
-import { removeCardFromHand } from "../../../game/StateUtils"
+import { GameState } from "../../../shared/gamestate/GameState"
 
 const cardScript: CardScript = {
+    // Play: Your opponent discards a random card from their hand.
     amber: () => 1,
     onPlay: {
-        perform: (state) => {
-            const opponentState = inactivePlayerState(state) as PlayerState
-
-            if (!opponentState.hand.length) {
-                return
-            }
-
-            const randomCardNum = Math.floor(Math.random() * opponentState.hand.length)
-            const randomCard = opponentState.hand[randomCardNum]
-            removeCardFromHand(opponentState, opponentState.hand[randomCardNum].id)
-            opponentState.discard.push(randomCard)
+        perform: (state: GameState, config: CardActionConfig) => {
+            //Add onPlay code here
         }
-    }
+    },
 }
-
 cardScripts.scripts.set("mind-barb", cardScript)
